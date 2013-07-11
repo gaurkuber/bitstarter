415039acd4b0cb088c9469b1697abe9c2d07ec6a
Kuber Gaur <gaurkuber@gmail.com>
2013-07-11 12:59:54 +0000

    web.js modified to UNuse buffer

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var express = require('express');
-var buffer = Buffer(fs.readFileSync('index.html', 'utf-8'));
+//var buffer = Buffer(fs.readFileSync('index.html', 'utf-8'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8', 0, 27));
+//  response.send(buffer.toString('utf-8', 0, 27));
+response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
