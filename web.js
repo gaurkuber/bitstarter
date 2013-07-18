5ae41ff58df1eb552ed651caf666be34cc1ce1ca
Kuber Gaur <gaurkuber@gmail.com>
2013-07-18 15:24:36 +0000

    web.js with port 8080

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var fs = require('fs');
 var express = require('express');
-//var buffer = Buffer(fs.readFileSync('index.html', 'utf-8'));
+var htmlfile="index.html";//var buffer = Buffer(fs.readFileSync('index.html', 'utf-8'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send(buffer.toString('utf-8', 0, 27));
-response.send(fs.readFileSync('index.html', 'utf-8'));
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
